89af11a158f18146a53987f96329019d8cc943c5
Henry Boyd <AmorphousCube@gmail.com>
2020-03-02 17:54:06 +0000

    refactored main to clean up duplicate code

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
 class Game
   def initialize
-    @active_player =1
+    @active_player = 0
   end
 
   attr_accessor :active_player
--- main.rb
+++ main.rb
@@ -3,51 +3,30 @@ load 'player.rb'
 load 'question.rb'
 
 g = Game.new
-p1 = Player.new
-p2 = Player.new
+players = [Player.new, Player.new]
 
-puts "Welcome to MATH GAME. Two players will answer very easy questions until you fail three times, embarrassing yourself forever."
+puts "Welcome to MATH GAME. Two players will answer very easy questions until one of you fails three times, embarrassing yourself forever."
 continue = true
 while(continue)
-  if g.active_player == 1
-    g.active_player = 2
-    puts "Player 1, your question is:"
-    q = Question.new
-    q.print_question
-    if q.check_answer(gets.chomp.to_i)
-      puts "correct"
-    else
-      puts "WRONG"
-      p1.lives -= 1
-      puts "Player 1 has #{p1.lives} lives remaining"
-      if p1.lives < 1
-        continue = false
-      end
-    end
+  
+  puts "Player #{g.active_player+1}, your question is:"
+  q = Question.new
+  q.print_question
+  if q.check_answer(gets.chomp.to_i)
+    puts "correct"
   else
-    g.active_player = 1
-    puts "Player 2, your question is:"
-    q = Question.new
-    q.print_question
-    if q.check_answer(gets.chomp.to_i)
-      puts "correct"
-    else
-      puts "WRONG"
-      p2.lives -= 1
-      puts "Player 2 has #{p2.lives} lives remaining"
-      if p2.lives < 1
-        continue = false
-      end
+    puts "WRONG"
+    players[g.active_player].lives -= 1
+    puts "Player #{g.active_player+1} has #{players[g.active_player].lives} lives remaining"
+    if players[g.active_player].lives < 1
+      continue = false
     end
   end
 
   if !continue
     puts "----GAME OVER----"
-    if p1.lives < 1
-      puts "Player 1 now lives in ETERNAL SHAME"
-    else
-      puts "Player 2 now lives in ETERNAL SHAME"
-    end
+    puts "Player #{g.active_player+1} now lives in ETERNAL SHAME"
   end
 
+  g.active_player = (g.active_player + 1)%2
 end
